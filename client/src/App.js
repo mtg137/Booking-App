a39754567a699b083a6b8a4775696e879e644d92
mtg137 <jony374@outlook.com>
2022-08-03 03:55:46 -0400

    add client

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="container">
+        <nav className="navbar navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand text-center" href="#">
+              <h2>Hotel Booking DApp</h2>
+            </a>
+          </div>
+        </nav>
+        <div className="row">
+            <div className="col-md-12">
+                <p>List Hotels Here</p>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
